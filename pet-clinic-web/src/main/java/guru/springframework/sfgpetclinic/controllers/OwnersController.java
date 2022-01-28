24fcc595b6186ea5fda4818760ef125183baa44f
nvijayakumar <vijay.be.kumar@gmail.com>
2022-01-28 16:17:21 +0530

    Listed all the owners from the map repository. Closes #3

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnersController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnersController.java
@@ -4,7 +4,10 @@
 package guru.springframework.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import guru.springframework.sfgpetclinic.services.OwnerService;
 
 /**
  * @author vijayakumar
@@ -14,9 +17,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/owners")
 @Controller
 public class OwnersController {
+	
+	private final OwnerService ownerService;
+	
+	public OwnersController(OwnerService ownerService) {
+		this.ownerService = ownerService;
+	}
 
 	@RequestMapping({"", "/index", "/index.html"})
-	public String ownersList() {
+	public String ownersList(Model model) {
+		System.out.println(model.getClass().getName());
+		model.addAttribute("owners", ownerService.findAll());
 		return "owners/index";
 	}
 }
