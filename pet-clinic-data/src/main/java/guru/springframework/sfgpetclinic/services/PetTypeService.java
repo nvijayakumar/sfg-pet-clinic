056cfc2a6fb29a762d7933d2d9f513c1039a293a
nvijayakumar <vijay.be.kumar@gmail.com>
2022-02-04 12:24:54 +0530

    Pet type service and map implementation added. closes #12

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.PetType;
+
+/**
+ * @author vijayakumar
+ * @Since  04-Feb-2022
+ *
+ */
+public interface PetTypeService extends CurdService<PetType, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package guru.springframework.sfgpetclinic.services.map;
+
+import guru.springframework.sfgpetclinic.model.PetType;
+import guru.springframework.sfgpetclinic.services.PetTypeService;
+
+/**
+ * @author vijayakumar
+ * @Since  04-Feb-2022
+ *
+ */
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+}
