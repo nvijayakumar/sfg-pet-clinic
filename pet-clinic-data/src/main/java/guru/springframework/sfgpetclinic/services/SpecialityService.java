9c9a5dc0bc3064484adf8a1dd8266ac8e19903b5
nvijayakumar <vijay.be.kumar@gmail.com>
2022-02-07 10:54:53 +0530

    Speciality service and map implementation added. closes #15.

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/SpecialityService.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+
+/**
+ * @author vijayakumar
+ * @Since  07-Feb-2022
+ *
+ */
+public interface SpecialityService extends CurdService<Speciality, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/SpecialityServiceMap.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package guru.springframework.sfgpetclinic.services.map;
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+import guru.springframework.sfgpetclinic.services.SpecialityService;
+
+/**
+ * @author vijayakumar
+ * @Since  07-Feb-2022
+ *
+ */
+public class SpecialityServiceMap extends AbstractMapService<Speciality, Long> implements SpecialityService {
+
+}
