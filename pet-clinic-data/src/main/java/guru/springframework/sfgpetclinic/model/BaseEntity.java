197dfaa2ad86cf0da2961c88d49ba7b8dfe0107e
nvijayakumar <vijay.be.kumar@gmail.com>
2022-02-12 18:55:57 +0530

    converted the BaseEntity to JPA entity.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -5,13 +5,21 @@ package guru.springframework.sfgpetclinic.model;
 
 import java.io.Serializable;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
 /**
  * @author vijayakumar
  * @Since  25-Jan-2022
  *
  */
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	public Long getId() {
