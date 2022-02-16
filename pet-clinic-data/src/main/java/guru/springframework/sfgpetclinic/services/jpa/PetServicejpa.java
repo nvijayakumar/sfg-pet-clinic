7ed457a121493f53517153f53e11242429f2e828
nvijayakumar <vijay.be.kumar@gmail.com>
2022-02-16 12:21:43 +0530

    spring data jpa added for Pet.

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/jpa/PetServicejpa.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package guru.springframework.sfgpetclinic.services.jpa;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import guru.springframework.sfgpetclinic.model.Pet;
+import guru.springframework.sfgpetclinic.repositories.PetRepository;
+import guru.springframework.sfgpetclinic.services.PetService;
+
+/**
+ * @author vijayakumar
+ * @Since  16-Feb-2022
+ *
+ */
+@Service
+@Profile("jpa")
+public class PetServicejpa extends AbstractJpaService<Pet, PetRepository> implements PetService {
+
+	public PetServicejpa(PetRepository repository) {
+		super(repository);
+	}
+
+}
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/jpa/PetTypeServiceJpa.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/jpa/PetTypeServiceJpa.java
@@ -3,6 +3,9 @@
  */
 package guru.springframework.sfgpetclinic.services.jpa;
 
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
 import guru.springframework.sfgpetclinic.model.PetType;
 import guru.springframework.sfgpetclinic.repositories.PetTypeRepository;
 import guru.springframework.sfgpetclinic.services.PetTypeService;
@@ -12,6 +15,8 @@ import guru.springframework.sfgpetclinic.services.PetTypeService;
  * @Since  16-Feb-2022
  *
  */
+@Service
+@Profile("jpa")
 public class PetTypeServiceJpa extends AbstractJpaService<PetType, PetTypeRepository> implements PetTypeService {
 
 	public PetTypeServiceJpa(PetTypeRepository repository) {
